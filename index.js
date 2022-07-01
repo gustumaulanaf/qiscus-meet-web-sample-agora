f015f585f57aa4ae217d8fb183e9ec64f323a89f
Gustu Maulana Firmansyah <gustumaulanaf@gmail.com>
2022-07-01 15:03:22 +0700

    simplify

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ import AgoraRTC from "agora-rtc-sdk-ng";
 
 let remoteContainer= document.getElementById("remote-container");
 let baseUrl = "https://agora-meet-server.herokuapp.com/api";
-let baseUrl2 = "https://api.agora.io/v1/apps";
 let token = "";
 let sid = "";
 let rid="";
 var uid = "";
 function addVideoContainer(uid){
-    document.getElementById("rid").innerText = "SEDANG DITAMBAHKAN"+uid;
-    let streamDiv=document.createElement("div"); // Create a new div for every stream
-    streamDiv.id=uid;                       // Assigning id to div
-    streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
-    remoteContainer.appendChild(streamDiv);      // Add new div to container
+    let streamDiv=document.createElement("div"); 
+    streamDiv.id=uid;            
+    streamDiv.style.transform="rotateY(180deg)"; 
+    remoteContainer.appendChild(streamDiv);     
 }
-/**
- * @name removeVideoContainer
- * @param uid - uid of the user
- * @description Helper function to remove the video stream from "remote-container"
- */
+
 function removeVideoContainer (uid) {
     let remDiv=document.getElementById(uid);
     remDiv && remDiv.parentNode.removeChild(remDiv);
@@ -44,39 +38,30 @@ async function getToken(channelName, uid){
 };
 
 document.getElementById("start").onclick = async function () {
-   
-    // Client Setup
-    // Defines a client for RTC
     const client = AgoraRTC.createClient({ mode: "rtc", codec: "h264" });
-    // Get credentials from the form
     let appId = document.getElementById("app-id").value;
     let channelId = document.getElementById("channel").value;
     token = await getToken(channelId,"0");
-    // Create local tracks
     const [localAudioTrack, localVideoTrack] = await AgoraRTC.createMicrophoneAndCameraTracks();
     document.getElementById("token").value = token;
-    // Initialize the stop button
     initStop(client, localAudioTrack, localVideoTrack);
-    
-    // Play the local track
     localVideoTrack.play('me');
 
-    // Set up event listeners for remote users publishing or unpublishing tracks
     client.on("user-published", async (user, mediaType) => {
-        await client.subscribe(user, mediaType); // subscribe when a user publishes
+        await client.subscribe(user, mediaType); 
         document.getElementById("rid").innerText = "SEDANG DITAMBAHKAN"+user.uid;
 
         if (mediaType === "video") {
-          addVideoContainer(String(user.uid)) // uses helper method to add a container for the videoTrack
+          addVideoContainer(String(user.uid)) 
           user.videoTrack.play(String(user.uid));
         }
         if (mediaType === "audio") {
-          user.audioTrack.play(); // audio does not need a DOM element
+          user.audioTrack.play();
         }
     });
     client.on("user-unpublished",  async (user, mediaType) => {
         if (mediaType === "video") {
-            removeVideoContainer(user.uid) // removes the injected container
+            removeVideoContainer(user.uid);
         }
     });
     document.getElementById("startRecording").disabled = false;
@@ -86,31 +71,29 @@ document.getElementById("start").onclick = async function () {
 
         });
       }, 1000) 
-    // Join a channnel and retrieve the uid for local user
     const _uid = await client.join(appId, channelId, token, null); 
     document.getElementById("uid").value = _uid;
     uid = _uid;
     await client.publish([localAudioTrack, localVideoTrack]);
-
 };
 
 function initStop(client, localAudioTrack, localVideoTrack){
     const stopBtn = document.getElementById('stop');
-    stopBtn.disabled = false; // Enable the stop button
-    stopBtn.onclick = null; // Remove any previous event listener
+    stopBtn.disabled = false; 
+    stopBtn.onclick = null;
     stopBtn.onclick = function () {
-        client.unpublish(); // stops sending audio & video to agora
-        localVideoTrack.stop(); // stops video track and removes the player from DOM
-        localVideoTrack.close(); // Releases the resource
-        localAudioTrack.stop();  // stops audio track
-        localAudioTrack.close(); // Releases the resource
+        client.unpublish(); 
+        localVideoTrack.stop();
+        localVideoTrack.close(); 
+        localAudioTrack.stop();  
+        localAudioTrack.close(); 
         client.remoteUsers.forEach(user => {
             if (user.hasVideo) {
-                removeVideoContainer(user.uid) // Clean up DOM
+                removeVideoContainer(user.uid) 
             }
-            client.unsubscribe(user); // unsubscribe from the user
+            client.unsubscribe(user); 
         });
-        client.removeAllListeners(); // Clean up the client object to avoid memory leaks
+        client.removeAllListeners(); 
         stopBtn.disabled = true;
     }
 }
